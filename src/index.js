7c1ae951a2c2a14a7c9185d7a1b8d3d6659bccd0
uyeong <uyeong21c@gmail.com>
2016-03-09 00:52:23 +0900

    refactor(index.js): exports a ICON_TYPE object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'core-js/fn/object/assign';
 import 'core-js/fn/promise';
-import PreloaderIcon from './PreloaderIcon';
+import PreloaderIcon, {ICON_TYPE} from './PreloaderIcon';
 
 module.exports = PreloaderIcon;
+module.exports.ICON_TYPE = ICON_TYPE;
