858a3489135e65f06b5e4282118fa43f691816e2
uyeong <uyeong21c@gmail.com>
2017-05-17 19:46:32 +0900

    Update Puff icon

--- src/loaders/Puff.js
+++ src/loaders/Puff.js
@@ -10,6 +10,7 @@ class Puff extends React.Component {
     constructor(props) {
         super(props);
 
+        this.radius = 50 - (props.strokeWidth / 2);
         this.stepper = new Stepper({
             duration: props.duration,
             easing: linear,
@@ -23,8 +24,8 @@ class Puff extends React.Component {
         this.stepper.on('update', (n) => {
             const n2 = n >= .5 ? n - .5 : n + .5;
 
-            c1.setAttribute('r', spread(n) * 20);
-            c2.setAttribute('r', spread(n2) * 20);
+            c1.setAttribute('r', spread(n) * this.radius);
+            c2.setAttribute('r', spread(n2) * this.radius);
             c1.style.strokeOpacity = 1 - fade(n);
             c2.style.strokeOpacity = 1 - fade(n2);
         });
@@ -33,9 +34,13 @@ class Puff extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
+        if (prevProps.strokeWidth !== this.props.strokeWidth) {
+            this.radius = 50 - (this.props.strokeWidth / 2);
+        }
+
         if (prevProps.duration !== this.props.duration) {
-            this.c1.setAttribute('r', 0);
-            this.c2.setAttribute('r', 0);
+            this.c1.setAttribute('r', '0');
+            this.c2.setAttribute('r', '0');
             this.c1.style.strokeOpacity = 0;
             this.c2.style.strokeOpacity = 0;
             this.stepper.option('duration', this.props.duration);
@@ -51,19 +56,18 @@ class Puff extends React.Component {
     }
 
     render() {
-        const strokeWidth = this.props.strokeWidth;
-        const viewBoxSize = 42 + strokeWidth;
-
         return (
             <div className="preloader-icon__puff" style={{height: '100%'}}>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}
+                    viewBox="0 0 102 102"
                     stroke={this.props.strokeColor}
                 >
-                    <g fill="none" strokeWidth={strokeWidth}>
-                        <circle ref={(el) => this.c1 = el} cx="22" cy="22" r="0" style={{strokeOpacity: 0}}/>
-                        <circle ref={(el) => this.c2 = el} cx="22" cy="22" r="0" style={{strokeOpacity: 0}}/>
+                    <g fill="none">
+                        <g transform="translate(1 1)" strokeWidth={this.props.strokeWidth}>
+                            <circle ref={(el) => this.c1 = el} cx="50" cy="50" r="0" style={{strokeOpacity: 0}}/>
+                            <circle ref={(el) => this.c2 = el} cx="50" cy="50" r="0" style={{strokeOpacity: 0}}/>
+                        </g>
                     </g>
                 </svg>
             </div>
